10e0da938a48ca6095345fca3cb05134fc53a1ee
Ahmed <ahmed@gmail.com>
2026-08-07 17:27:30 +0530

    ecossytem change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "infrastructure-monitor",
+      name: "frontend",
       cwd: "./",
       script: "node_modules/next/dist/bin/next",
       args: "start",
